db1e147933acdb0eb307dd035cf60a270bda6569
Kartik Singhal <kartiksinghal@gmail.com>
2013-03-16 15:28:06 +0530

    Add Quote model

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Quote(models.Model):
+    message = models.TextField()
+    name = models.CharField(max_length=100)
+    PROGRAM_CHOICES = (
+            ('BCS', 'BTech'),
+            ('MCS', 'MTech'),
+            ('MCA', 'MCA'),
+    )
+    program = models.CharField(max_length=3,
+                               choices = PROGRAM_CHOICES,
+                               default = 'BCS')
+    class_of = models.IntegerField("Enter year of passing out")
+    submission_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.message
