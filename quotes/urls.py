027e1a80afae23e4a90bad0fff5f54b8e4ac116f
Kartik Singhal <kartiksinghal@gmail.com>
2013-03-17 16:51:49 +0530

    Switch to generic views

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,10 +1,22 @@
 from django.conf.urls import patterns, url
+from django.views.generic import DetailView, ListView
+
+from quotes.models import Quote
 
 from quotes import views
 
 urlpatterns = patterns('',
         # ex: quotes/
-        url(r'^$', views.index, name='index'),
+        url(r'^$',
+            ListView.as_view(
+                queryset=Quote.objects.order_by('-submission_time')[:4],
+                context_object_name='latest_quote_list',
+                template_name='quotes/index.html'),
+            name='index'),
         # ex: /quotes/2/
-        url(r'^(?P<quote_id>\d+)/$', views.detail, name='detail'),
+        url(r'^(?P<pk>\d+)/$',
+            DetailView.as_view(
+                model=Quote,
+                template_name='quotes/detail.html'),
+            name='detail'),
 )
--- quotes/views.py
+++ quotes/views.py
@@ -2,11 +2,3 @@ from django.shortcuts import render, get_object_or_404
 
 from quotes.models import Quote
 
-def index(request):
-    latest_quote_list = Quote.objects.order_by('-submission_time')[:4]
-    context = {'latest_quote_list': latest_quote_list}
-    return render(request, 'quotes/index.html', context)
-
-def detail(request, quote_id):
-    quote = get_object_or_404(Quote, pk=quote_id)
-    return render(request, 'quotes/detail.html', {'quote':quote})
